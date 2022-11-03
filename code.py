da01dc3b832d747e7755bd3e699d54bc0896fd39
injoo626 <57341200+injoo626@users.noreply.github.com>
2022-11-03 14:30:14 +0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,48 @@
+import time
+
+import board
+import analogio
+
+
+# Initialize analog input connected to photocell.
+photocell = analogio.AnalogIn(board.A1)
+
+# Make a function to convert from analog value to voltage.
+def analog_voltage(adc):
+    return adc.value / 65535 * adc.reference_voltage
+
+ambientLight = []
+oldVal = None
+newVal = None
+intDiff = 0
+mode = 0
+
+# Main loop reads value and voltage every second and prints them out.
+while True:
+    # Read the light intensity and the voltage.
+    newVal = photocell.value
+    volts = analog_voltage(photocell)
+    # Print the values:
+    print('Photocell value: {0} voltage: {1}V'.format(newVal, volts))
+
+    # Calculate the difference in intensity
+    if oldVal is not None:
+        intDiff = newVal - oldVal
+        print(intDiff)
+
+    # Update the latested measured value
+    oldVal = newVal
+
+    # Continue if the difference is smaller than 3000
+    if abs(intDiff) <3000:
+        print(mode)
+        time.sleep(2.0)
+
+
+    # If the difference is larger than 3000
+    # Change mode, wait for 10 seconds, mode changes back to initial
+    else:
+        mode = 1
+        print(mode)
+        time.sleep(10.0)
+        mode = 0
