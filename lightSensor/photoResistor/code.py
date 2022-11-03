aa450c11dd266e3ea7a21755bd43f3571419db9c
injoo626 <57341200+injoo626@users.noreply.github.com>
2022-11-03 13:50:40 +0400

    Add files via upload

--- lightSensor/photoResistor/code.py
+++ lightSensor/photoResistor/code.py
@@ -12,15 +12,21 @@ def analog_voltage(adc):
     return adc.value / 65535 * adc.reference_voltage
 
 ambientLight = []
+oldVal = 0
+newVal = 0
 
 # Main loop reads value and voltage every second and prints them out.
 while True:
-    # Read the value, then the voltage.
-    val = photocell.value
+    # Read the light intensity and the voltage.
+    newVal = photocell.value
     volts = analog_voltage(photocell)
     # Print the values:
-    print('Photocell value: {0} voltage: {1}V'.format(val, volts))
-    # Delay for a second and repeat!
-    ambientLight.append(val)
-    print(ambientLight)
+    print('Photocell value: {0} voltage: {1}V'.format(newVal, volts))
+    # Calculate the difference in intensity
+    if oldVal is not None:
+        intDifference = newVal - oldVal
+    print(intDifference)
+    # Update the latest measured intensity
+    oldVal = newVal
+    # Wait for five seconds to repeat
     time.sleep(5.0)
