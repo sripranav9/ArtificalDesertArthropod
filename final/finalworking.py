15dc1b6cb05d88b83763314ae488e4ca76bc583d
Sri Pranav Srivatsavai <92122776+sripranav9@users.noreply.github.com>
2022-11-24 22:17:03 +0400

    Create finalworking.py

---
+++ final/finalworking.py
@@ -0,0 +1,148 @@
+from adafruit_motorkit import MotorKit
+import time
+import board
+import analogio
+from adafruit_simplemath import map_range
+
+
+# ------------
+# Intialize the Motors
+#kit = MotorKit(i2c=board.I2C())
+
+# Initialize analog input connected to photocell.
+microphone = analogio.AnalogIn(board.A5)
+
+# Initialize potentiometer connected to A3, power & ground
+potentiometer = analogio.AnalogIn(board.A3)
+
+# Initialize light sensor connected to A1
+photocell = analogio.AnalogIn(board.A1)
+# ------------
+
+# --------------
+# Make a function to convert from analog value to voltage.
+def analog_voltage(adc):
+    return adc.value / 65535 * adc.reference_voltage
+
+# Function to convert the potentiometer values to volts
+def get_voltage(pin):
+    return (pin.value * 3.3) / 65536
+# --------------
+
+# set the min and max threshold values of the microphone
+potentiomter_threshold_min = 450.0
+potentiomter_threshold_max = 800.0
+
+# initilialize variables to store the values in the while loop for comparison
+oldSoundVal = 0
+newSoundVal = 0
+    # threshold = 450.0 - # this will be replaced by the potentiometer reading as the threshold
+
+# 0 for default checking the values, once there is a difference, go to mode 1
+# mode 1 - waits for sometime for the arthropod to perform the action and then go ahead for checking again
+#sound sensor variables
+soundDiff = 0
+soundMode = 0
+sleepTime = 5.0  # for mode 1 - seconds to wait for
+
+# to keep track of the sound intDiff 's initated, and skip the first value - printing first value not reqd.
+count = 0
+
+#light sensor variables
+oldVal = None
+newVal = None
+intDiff = 0
+
+
+#testing time monotonic
+startTime = time.monotonic()
+
+# Main loop reads value and voltage every second and prints them out.
+while True:
+
+
+    # --
+    # microphone
+    # convert to arduino 10-bit [1024] fromhere 16-bit [65536]
+    sample = microphone.value / 64
+
+    # equate the sample to newVal to compare in further iterations
+    newSoundVal = sample
+
+    #print('The current sound value is: ', newSoundVal)
+    if oldSoundVal is not None:
+        soundDiff = newSoundVal - oldSoundVal
+
+    # equating this for the next iteration
+    oldSoundVal = newSoundVal
+
+    # --
+    # potentiometer
+    voltage = get_voltage(potentiometer)      # store the potentiometer voltage
+    
+    # map the values from the potentiometer to 
+    mapped_thresholdValue = int(map_range(voltage, 0,3.1, potentiomter_threshold_min, potentiomter_threshold_max))
+    #print(mapped_thresholdValue)
+    newVal = photocell.value
+    # volts = analog_voltage(photocell)
+    # Print the values:
+    # print('Photocell value: {0} voltage: {1}V'.format(newVal, volts))
+
+    # Calculate the difference in light intensity
+    if oldVal is not None:
+        intDiff = newVal - oldVal
+        #print(intDiff)
+
+    # Update the latested measured value
+    oldVal = newVal
+
+    #print(mapped_thresholdValue)
+    #time.sleep(0.3)
+    if (count!=0) and (soundDiff > mapped_thresholdValue or soundDiff < (-mapped_thresholdValue)):
+        print('hey')
+        print('\nThreshold currently set at:',mapped_thresholdValue)
+        print("Change in sound detected, value = ", soundDiff)
+        soundMode = 1;
+        print('Activated StandBy','\n')
+        #kit.motor1.throttle = 0.75
+        #kit.motor3.throttle = 0.75
+        #time.sleep(5.0)
+        time.sleep(sleepTime)
+        print('Deactivated StandBy')
+    else:
+        #kit.motor1.throttle = -0.5
+        #kit.motor3.throttle = -0.5
+        #time.sleep(2)
+        soundMode = 0
+        #print(soundDiff)
+        #time.sleep(0.5)
+    
+    # to skip the first extra value
+    if (count == 0):
+        count+=1;
+    
+    #LIGHT code
+    # Continue if the difference is smaller than 3000
+    
+    #checking the current time 
+    currTime = time.monotonic()
+    if ((currTime - startTime) > 1.0 ):
+        
+        if abs(intDiff) > 3000:
+        #    kit.motor1.throttle = -0.50
+        #    kit.motor3.throttle = -0.50
+            #mode = 1
+            print("\nChange in light detected, value = ", intDiff)
+            print('Activated StandBy (Mode 1)')
+        #    kit.motor1.throttle = 0.75
+        #    kit.motor3.throttle = 0.75
+            time.sleep(5.0)
+            print('Deactivated StandBy, checking for changes in light...\n')
+
+# If the difference is larger than 3000
+# Change mode, wait for 10 seconds, mode changes back to initial
+        else:
+            mode=0
+            #time.sleep(1.0)
+            #print(mode)
+    
